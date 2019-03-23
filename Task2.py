732112dcb5245f06b2865af2958d01c08b503e62
Rahul <rahulatrkm@gmail.com>
2019-03-24 01:34:10 +0530

    Project Completed, time to get reviewed

---
+++ Task2.py
@@ -0,0 +1,27 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 2: Which telephone number spent the longest time on the phone
+during the period? Don't forget that time spent answering a call is
+also time spent on the phone.
+Print a message:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during
+September 2016.".
+"""
+telephone_numbers = dict()
+for i in range(len(calls)):
+    telephone_numbers[calls[i][0]] = telephone_numbers.get(calls[i][0], 0) + int(calls[i][-1])
+    telephone_numbers[calls[i][1]] = telephone_numbers.get(calls[i][1], 0) + int(calls[i][-1])
+number_with_max_time = sorted(telephone_numbers, reverse=True, key=lambda x: telephone_numbers[x])[0]
+print(str(number_with_max_time) + " spent the longest time, " + str(telephone_numbers[number_with_max_time]) + " seconds, on the phone during September 2016.")
---
+++ Task4.py
@@ -0,0 +1,45 @@
+"""
+Read file into texts and calls.
+It's ok if you don't understand how to read files.
+"""
+import csv
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+TASK 4:
+The telephone company want to identify numbers that might be doing
+telephone marketing. Create a set of possible telemarketers:
+these are numbers that make outgoing calls but never send texts,
+receive texts or receive incoming calls.
+
+Print a message:
+"These numbers could be telemarketers: "
+<list of numbers>
+The list of numbers should be print out one per line in lexicographic order with no duplicates.
+"""
+telephone_numbers = set()
+for i in range(len(calls)):
+    telephone_numbers.add(calls[i][0])
+    try:
+        telephone_numbers.remove(calls[i][1])
+    except:
+        continue
+
+for i in range(len(texts)):
+    try:
+        telephone_numbers.remove(texts[i][0])
+    except:
+        pass
+    try:
+        telephone_numbers.remove(texts[i][1])
+    except:
+        continue
+
+print(telephone_numbers)
